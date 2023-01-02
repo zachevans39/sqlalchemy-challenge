fe44099bf55c5882a38a335dca4ff0f2e069d58d
ZacharyEvans <109036870+zachevans39@users.noreply.github.com>
2023-01-01 18:16:12 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+# Create engine and reflect
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# View all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+Station = Base.classes.station
+Measurement = Base.classes.measurement
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
